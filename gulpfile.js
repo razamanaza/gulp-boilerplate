33a5008f7537f25d07fe8f89f0c3d0bdb9a764cb
razamanaza <razamanaza@yandex.ru>
2019-05-31 16:53:13 +1000

    Add system notifications on sass compilation errors

--- gulpfile.js
+++ gulpfile.js
@@ -45,6 +45,7 @@ var del = require('del');
 var flatmap = require('gulp-flatmap');
 var lazypipe = require('lazypipe');
 var rename = require('gulp-rename');
+var notify = require("gulp-notify");
 
 // Styles
 var sass = require('gulp-sass');
@@ -90,7 +91,7 @@ var buildStyles = function (done) {
 		.pipe(sass({
 			outputStyle: 'expanded',
 			sourceComments: true
-		}))
+		}).on("error", notify.onError()))
 		.pipe(prefix({
 			browsers: ['last 2 version', '> 0.25%'],
 			cascade: true,
